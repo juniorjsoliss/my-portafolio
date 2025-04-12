6d6a57786090d467437ad7ff65a2346009348f27
Junior Solis <juniorjsolis@gmail.com>
2025-04-11 23:45:30 -0600

    feat(custom): add new configuration in component navbar and modifications in layout

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+
+    return (
+        <nav className="w-full bg-purple-700 text-white p-4 flex justify-between items-center">
+            <h1 className="text-xl font-bold">Developer JuniorjSolis💀</h1>
+            <button className="md:hidden">🍔</button>
+        </nav>
+    )
+}
